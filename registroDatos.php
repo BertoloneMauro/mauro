d1ed8690883568e3dcb5a60a5c434d907cdc96ed
BertoloneMauro <146277778+BertoloneMauro@users.noreply.github.com>
2023-10-17 18:20:57 -0300

    Update registroDatos.php

--- registroDatos.php
+++ registroDatos.php
@@ -1,5 +1,6 @@
 <?php
-var_dump($_POST);
+session_start(); // Inicia la sesión para mostrar mensajes al usuario
+
 // Conexión a la base de datos
 $conexion = mysqli_connect("localhost", "root", "", "usuarioinfo");
 
@@ -8,24 +9,30 @@ if (!$conexion) {
     die("Error de conexión: " . mysqli_connect_error());
 }
 
-// Recopila los datos del formulario
-$email = $_POST['email'];
-$contrasena = $_POST['contrasena'];
-$documento = $_POST['nDocumento'];
-$nombre = $_POST['nombre'];
+// Sanitiza y recopila los datos del formulario
+$email = mysqli_real_escape_string($conexion, $_POST['email']);
+$contrasena = password_hash($_POST['contrasena'], PASSWORD_DEFAULT); // Almacena la contraseña de manera segura
+$documento = mysqli_real_escape_string($conexion, $_POST['nDocumento']);
+$nombre = mysqli_real_escape_string($conexion, $_POST['nombre']);
+
+// Configura la variable de sesión $_SESSION['email'] con el correo electrónico del usuario
+$_SESSION['email'] = $email;
 
 // Realiza una consulta SQL para insertar los datos en la tabla "datos"
 $query = "INSERT INTO datos (contraseña, documento, email, nombre) 
           VALUES ('$contrasena', '$documento', '$email', '$nombre')";
 
 if (mysqli_query($conexion, $query)) {
-    header("Location: menu.html"); // Utiliza "Location" y la URL completa
+    $_SESSION['message'] = "Registro exitoso"; // Mensaje de éxito
+    header("Location: menu.html"); // Redirige a la página de inicio
     exit;
 } else {
-    echo "Error al registrar el usuario: " . mysqli_error($conexion);
+    $_SESSION['error'] = "Error al registrar el usuario: " . mysqli_error($conexion); // Mensaje de error
+    header("Location: registrarse.html"); // Redirige de nuevo al formulario de registro
+    exit;
 }
-
 
 // Cierra la conexión a la base de datos
 mysqli_close($conexion);
 ?>
+
